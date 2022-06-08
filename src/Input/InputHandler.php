b7601480dbf3730aa8aa3ecb57846f1e267e408c
Mateusz <mateuszskrzypiec11@gmail.com>
2022-06-08 14:46:01 +0200

    The InputHandler class was created

---
+++ src/Input/InputHandler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace VendingMachine\Input;
+
+use VendingMachine\Exception\InvalidInputException;
+
+class InputHandler implements InputHandlerInterface
+{
+    public function getInput(): InputInterface
+    {
+        // TODO: Implement getInput() method.
+    }
+}
